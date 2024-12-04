806f864c3d43d3ba0b700ea813f6b8a2583d1c4c
Sam Shum <25998453+supercrazysam@users.noreply.github.com>
2024-12-04 12:31:48 -0600

    Update test_dutycycle_receie.ino

--- test_dutycycle_receie/test_dutycycle_receie.ino
+++ test_dutycycle_receie/test_dutycycle_receie.ino
@@ -11,8 +11,7 @@ void loop() {
     // Measure the length of the HIGH pulse (in microseconds)
     duration = pulseIn(pwmInputPin, HIGH);
 
-    // Calculate the duty cycle in percentage (assuming a 50Hz PWM signal)
-    // Adjust the denominator if using a different frequency
+
     dutyCycle = duration / 2040.82; // mega 2560 normal pin by default = 490hz   The period for a 490 Hz signal is approximately 2040.82 microseconds
     dutyCycle = constrain( dutyCycle, 0.0, 1.0);
     //dutyCycle = duration / 1020.41;   //since pin 13 of mega 2560 = 980hz
